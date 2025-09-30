e3d88ebba720f5b545cef58590db905dd6309d99
Baki Güneş <baki.gunes@netmera.com>
2025-09-30 16:33:04 +0300

    Add Netmera-Config.plist for initialize params

--- NetmeraFintech/Analytics/NetmeraHelper.swift
+++ NetmeraFintech/Analytics/NetmeraHelper.swift
@@ -9,15 +9,6 @@ import NetmeraNotification
 import NetmeraNotificationInbox
 
 class NetmeraHelper {
-    private static let NETMERA_API_KEY = "RH9qH4AIng0xmH-dqrh3SYTSU3Kw3OrQpqAkyF-u6YOgwJJt_7n55WtPYT3ak-U_"
-
-    public static func initNetmera() {
-        let netmeraParams = NetmeraParams(
-            apiKey: NETMERA_API_KEY
-        )
-        Netmera.initialize(params: netmeraParams)
-        Netmera.setLogLevel(.debug) // Options can be .debug, .info, .error, .fault
-    }
 
     // Request push notification authorization from user by calling the following method in an appropriate place:
     public static func requestNotificationPermission() {
